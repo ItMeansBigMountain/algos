f8b3b8436922fa029bdf67bd9443267a6b8d2d4d
itMeansBigMountain <affan.fareed@gmail.com>
2023-11-24 19:42:04 -0600

    search methods

--- FreeCodeCamp/LinkedList.py
+++ FreeCodeCamp/LinkedList.py
@@ -9,9 +9,6 @@ class Node:
         return f"<{self.data}>"
 
 
-
-
-
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -46,17 +43,53 @@ class LinkedList:
             position += 1
             current_node = current_node.next_node
 
-
-
-    def search(self, key):
+    def removeByKey(self, key):
         current_node = self.head
-        found = False
+        previous_node = None
+        while current_node != None :
+            if key == current_node.data and current_node is self.head :
+                self.head = current_node.next_node
+                return current_node
+            elif key == current_node.data and current_node is not self.head :
+                previous_node.next_node = current_node.next_node
+                return current_node
+            else:
+                previous_node = current_node
+                current_node = current_node.next_node
+        return None
+
+    def removeByIndex(self, index):
+        current_node = self.head
+        position = 0
+
+        if index > self.size(): return None
+        if index == 0: self.head = self.head.next_node
+        while current_node != None:
+            if position == index-1:
+                target = current_node.next_node
+                current_node.next_node = target.next_node
+                return current_node
+            position += 1
+            current_node = current_node.next_node
+        return None
+
+    def searchByKey(self, key):
+        current_node = self.head
         index = 0
         while current_node != None:
             if current_node.data == key:
-                found = True
                 return index
             index += 1
+            current_node = current_node.next_node
+        return None
+
+    def searchByIndex(self, index):
+        current_node = self.head
+        position = 0
+        while current_node != None:
+            if position == index:
+                return current_node.data
+            position += 1
             current_node = current_node.next_node
         return None
 
@@ -93,9 +126,16 @@ if __name__ == "__main__":
 
 
     
-    print("\nSEARCHING LINKED LIST...")
-    found_index =  l.search(10)
-    print("Found node at index:" ,found_index)
+    print("\nSEARCHING LINKED LIST BY VALUE...")
+    s = 10
+    found_index =  l.searchByKey(s)
+    print(f"Found node value {s} at index: {found_index}")
+
+    
+    print("\nSEARCHING LINKED LIST BY INDEX...")
+    i = 3
+    found_value =  l.searchByIndex(i)
+    print(f"Found node index {i} as value: {found_value}")
 
 
 
@@ -105,5 +145,20 @@ if __name__ == "__main__":
     l.insert(3,3)
     l.insert(3,3)
     l.insert(3,3)
+    l.insert(3,3)
+    print(l)
+    print("size:", l.size())
+
+
+
+    print("\nREMOVING FROM LINKED LIST BY INDEX (looped)...")
+    for x in range(5): l.removeByIndex(x)
+    print(l)
+    print("size:", l.size())
+
+
+
+    print("\nREMOVING FROM LINKED LIST BY VALUE...")
+    l.removeByKey(3)
     print(l)
     print("size:", l.size())
